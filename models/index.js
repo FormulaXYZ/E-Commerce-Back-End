f4430cee0d30024775f727a0602e3d56aeb478da
FormulaXYZ <thomas.assefa6@gmail.com>
2022-09-11 11:35:40 -0700

    worked on index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongTo(Category,{
+  foreignkey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasmany(Product, {
+  foreignkey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag,{
+  through: "ProductTag",
+});
 
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongTomant(Product,{
+  through: "ProductTag",
+});
 
 module.exports = {
   Product,
