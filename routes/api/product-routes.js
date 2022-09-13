1bfedb6e4843faf81b9851db2834a8c52ca92003
FormulaXYZ <thomas.assefa6@gmail.com>
2022-09-13 10:39:47 -0700

    product-routes.js delete one product by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -113,5 +113,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await  Product.destroy( {where: {
+      id:req.params.id
+    }} )
   
+    res.json(productData)
+  } catch(err){
+      res.status(500).json(err);
+  }
+});
+
 module.exports = router;
