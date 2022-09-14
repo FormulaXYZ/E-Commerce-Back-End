c99756f5468b9c79f01ebe40fd8f06d06282adb3
FormulaXYZ <thomas.assefa6@gmail.com>
2022-09-14 11:57:38 -0700

    fixed some syntax errors

--- models/index.js
+++ models/index.js
@@ -7,6 +7,7 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category,{
   foreignKey: 'category_id',
+  onDelete:"CASCADE"
 });
 // Categories have many Products
 Category.hasMany(Product, {
@@ -15,14 +16,14 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,{
   foreignKey:'product_id',
-  through: "ProductTag",
+  through: ProductTag,
 });
 
 // Tags belongToMany Products (through ProductTag)
 
 Tag.belongsToMany(Product,{
   foreignKey:'tag_id',
-  through: "ProductTag",
+  through: ProductTag,
 });
 
 module.exports = {
